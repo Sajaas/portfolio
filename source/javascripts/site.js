f126388a05009f94b1043fe97352b0af330d173a
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-12 19:15:05 +0200

    changed colors

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,18 +1,8 @@
-// // $(document).ready(function(){
-// //   $('.target').pushpin({
-// //     top: 0,
-// //     bottom: 1000,
-// //     offset: 0
-// //   });
-// // });
-//
-// $('.pushpin-demo-nav').each(function() {
-//   var $this = $(this);
-//   var $target = $('#' + $(this).attr('data-target'));
-//   $this.pushpin({
-//     top: $target.offset().top,
-//     bottom: $target.offset().top + $target.outerHeight() - $this.height()
-//   });
-// });
-
-$(".button-collapse").sideNav();
+$('.pushpin-live').each(function() {
+  var $this = $(this);
+  var $target = $('#' + $(this).attr('data-target'));
+  $this.pushpin({
+    top: $target.offset().top,
+    bottom: $target.offset().top + $target.outerHeight() - $this.height()
+  });
+});
