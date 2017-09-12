d7cb6863fc9758cf5ab3b486b9d4f8224e68d040
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-12 11:45:34 +0200

    some more styling, first try at navbar

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,16 +1,16 @@
-// $(document).ready(function(){
-//   $('.target').pushpin({
-//     top: 0,
-//     bottom: 1000,
-//     offset: 0
+// // $(document).ready(function(){
+// //   $('.target').pushpin({
+// //     top: 0,
+// //     bottom: 1000,
+// //     offset: 0
+// //   });
+// // });
+//
+// $('.pushpin-demo-nav').each(function() {
+//   var $this = $(this);
+//   var $target = $('#' + $(this).attr('data-target'));
+//   $this.pushpin({
+//     top: $target.offset().top,
+//     bottom: $target.offset().top + $target.outerHeight() - $this.height()
 //   });
 // });
-
-$('.pushpin-demo-nav').each(function() {
-  var $this = $(this);
-  var $target = $('#' + $(this).attr('data-target'));
-  $this.pushpin({
-    top: $target.offset().top,
-    bottom: $target.offset().top + $target.outerHeight() - $this.height()
-  });
-});
