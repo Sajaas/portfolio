c2dd4f67fe738bc8b80f04fbf31adc1d59bace90
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-13 12:55:49 +0200

    added email show on hover, pushpin navbars

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,19 +1,33 @@
 $(document).ready(function(){
-  $('.collapsible').collapsible();
+  $('.target').pushpin({
+    top: 0,
+    bottom: 1000,
+    offset: 0
+  });
+  if ($('.pushpin-demo-nav').length) {
+    $('.pushpin-demo-nav').each(function() {
+      var $this = $(this);
+      var $target = $('#' + $(this).attr('data-target'));
+      $this.pushpin({
+        top: $target.offset().top,
+        bottom: $target.offset().top + $target.outerHeight() - $this.height()
+      });
+    });
+  }
+  $('.hover').mouseover(function() {
+    $('.text').css("visibility","visible");
+  });
+
+  $('.hover').mouseout(function() {
+    $('.text').css("visibility","hidden");
+  });
+
 });
 
-// $(document).ready(function(){
-//   $('.target').pushpin({
-//     top: 0,
-//     bottom: 1000,
-//     offset: 0
-//   });
-//   $('.pushpin-live').each(function() {
-//     var $this = $(this);
-//     var $target = $('#' + $(this).attr('data-target'));
-//     $this.pushpin({
-//       top: $target.offset().top,
-//       bottom: $target.offset().top + $target.outerHeight() - $this.height()
-//     });
-//   });
-// });
+function copyToClipboard(element) {
+var $temp = $("<input>");
+$("body").append($temp);
+$temp.val($(element).html()).select();
+document.execCommand("copy");
+$temp.remove();
+}
