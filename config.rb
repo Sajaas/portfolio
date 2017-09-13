b3bbd553111f2a5a7437241c639dcec8e83ea369
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-13 17:24:27 +0200

    spellcheck

--- config.rb
+++ config.rb
@@ -7,6 +7,18 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.deploy_method = :git
+end
+
+require 'extensions/build_cleaner'
+
+configure :build do
+  activate :relative_assets
+  activate :build_cleaner
+end
+
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
