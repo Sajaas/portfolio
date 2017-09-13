6b493817fee561a388386c72a76c1ad5d06db059
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-13 08:44:47 +0200

    added collapsible on projects

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,8 +1,15 @@
-$('.pushpin-live').each(function() {
-  var $this = $(this);
-  var $target = $('#' + $(this).attr('data-target'));
-  $this.pushpin({
-    top: $target.offset().top,
-    bottom: $target.offset().top + $target.outerHeight() - $this.height()
-  });
-});
+// $(document).ready(function(){
+//   $('.target').pushpin({
+//     top: 0,
+//     bottom: 1000,
+//     offset: 0
+//   });
+//   $('.pushpin-live').each(function() {
+//     var $this = $(this);
+//     var $target = $('#' + $(this).attr('data-target'));
+//     $this.pushpin({
+//       top: $target.offset().top,
+//       bottom: $target.offset().top + $target.outerHeight() - $this.height()
+//     });
+//   });
+// });
