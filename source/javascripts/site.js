63fd3c33f903b3e9520b9eb0594c75f4b111d90a
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-14 23:29:41 +0200

    fixed overflowing navbar title

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,33 +1,18 @@
 $(document).ready(function(){
-  // $('.target').pushpin({
-  //   top: 0,
-  //   bottom: 1000,
-  //   offset: 0
-  // });
-  // if ($('.pushpin-demo-nav').length) {
-  //   $('.pushpin-demo-nav').each(function() {
-  //     var $this = $(this);
-  //     var $target = $('#' + $(this).attr('data-target'));
-  //     $this.pushpin({
-  //       top: $target.offset().top,
-  //       bottom: $target.offset().top + $target.outerHeight() - $this.height()
-  //     });
-  //   });
-  // }
+  $(".button-collapse").sideNav();
   $('.hover').mouseout(function() {
     $('.text').css("visibility","hidden");
   });
   $('.hover').mouseenter(function() {
     $('.text').css("visibility","visible");
   });
-  $(".button-collapse").sideNav();
   $('.collapsible').collapsible();
 });
 
 function copyToClipboard(element) {
-var $temp = $("<input>");
-$("body").append($temp);
-$temp.val($(element).html()).select();
-document.execCommand("copy");
-$temp.remove();
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).html()).select();
+  document.execCommand("copy");
+  $temp.remove();
 }
