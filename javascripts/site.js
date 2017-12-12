7f2160990acd40dab97a3d001c9b7f9fa36d2ad2
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-12-12 12:33:16 +0100

    Automated commit at 2017-12-12 11:33:16 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/site.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.hover').mouseout(function() {
+    $('.text').css("visibility","hidden");
+  });
+  $('.hover').mouseenter(function() {
+    $('.text').css("visibility","visible");
+  });
+  $('.collapsible').collapsible();
+  $('.parallax').parallax();
+  $(".button-collapse").sideNav({
+    closeOnClick: true
+  });
+});
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).html()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+;
